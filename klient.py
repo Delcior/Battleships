8f9597ed0fd8817d204d47293af8130f797c3d64
Delcior <kacper.derlatka@outlook.com>
2021-06-16 23:27:06 +0200

    rsa key exchange, class for map and first iteration of algorithm for map creation

--- klient.py
+++ klient.py
@@ -3,27 +3,180 @@ Klient TCP
 '''
 import socket
 import numpy as np
-CRLF = b"\r\n"
+import pickle
+import random
+CRLF = b"\r\n\r\n"
+
+class Map:
+    def __init__(self):
+        self.mapa = np.zeros((10, 10))
+        self.pozycje = []
+        self._notAvailable = set()
+        self._boats = {1:4, 2:3, 3:2, 4:1}
+
+    def buildMap(self):
+
+        pass
+
+    def _printInfo(self):
+        msg_choice = "Masz do wyboru:\n" \
+              " czteromasztowców: {czt}\n" \
+              " trójmasztowców: {czy}\n" \
+              " dwumasztowców:{dwa}\n" \
+              " jednomasztowców: {jed}".format(
+            czt=self._boats[4],
+            czy=self._boats[3],
+            dwa=self._boats[2],
+            jed=self._boats[1])
+
+        msg_options = "\n ====Menu====\n" \
+                      "[1] rozstaw jednomasztowiec\n" \
+                      "[2] rozstaw dwumasztowiec\n" \
+                      "[3] rozstaw trójmasztwoiec\n" \
+                      "[4] roztaw czwórmasztwiec\n" \
+                      "[enter] Zaakceptuj (brak statków zostanie uzupełniony losowo)"
+        print(self.mapa)
+        print(msg_choice)
+        print(msg_options)
+    def generateMap(self):
+        # for i in range(4, 0, -1):
+        #     self._placeBoats(i)
+        #     print("======po ulozeniu %d" % (i,))
+        #     print(self.mapa)
+        #     print("===================")
+        self._placeBoats(4)
+        print("======po ulozeniu %d" % (4,))
+        print(self.mapa)
+        print("===================")
+
+    def _placeBoats(self, length):
+        while self._boats[length] > 0:
+            x = random.randint(0, 10)
+            y = random.randint(0, 10)
+            coords = (x, y)#np.array([x, y])
+
+            if coords not in self._notAvailable:
+                #0 - góra
+                #1 - prawo itd..
+                for i in range(4):
+                    flag, points, map = self._chooseDirection(coords, i, length)
+                    if flag:
+                        self.mapa = map
+                        self._excludePoints(points)
+                        self._boats[length]-=1
+                        break
+
+
+
+    def _chooseDirection(self, point, direction, len):
+        if not self._isGood(point):
+            return False, None, None
+        #TODO: USUN MAP_COPY :)
+        map_copy = self.mapa
+        points = {point}
+        map_copy[point[0], point[1]] = 1
+        print(point, direction, len)
+        if direction == 0:
+            for i in range(len-1):
+                print(i)
+                point = (point[0], point[0]+1)
+                if not self._isGood(point):
+                    return False, None, None
+                print((point[0], point[0]+1))
+                points.add(point)
+                map_copy[point[0], point[1]] = 1
+                points = self._addNeighbours(point, points)
+        elif direction == 1:
+            for i in range(len-1):
+                point = (point[0]+1, point[0])
+                if not self._isGood(point):
+                    return False, None, None
+                points.add(point)
+                map_copy[point[0], point[1]] = 1
+                points = self._addNeighbours(point, points)
+        elif direction == 2:
+            for i in range(len-1):
+                point = (point[0], point[0]-1)
+                if not self._isGood(point):
+                    return False, None, None
+                points.add(point)
+                map_copy[point[0], point[1]] = 1
+                points = self._addNeighbours(point, points)
+        elif direction == 3:
+            for i in range(len-1):
+                point = (point[0]-1, point[0])
+                if not self._isGood(point):
+                    return False, None, None
+                points.add(point)
+                map_copy[point[0], point[1]] = 1
+                points = self._addNeighbours(point, points)
+
+        return True, points, map_copy
+
+
+    def _isGood(self, coords):
+        return coords not in self._notAvailable and (0 <= coords[0] < 10 and 0 <=coords[1] < 10)
+
+
+    def _addNeighbours(self, point, points):
+        points.add((point[0]+1, point[1]))
+        points.add((point[0]+1, point[1]+1))
+        points.add((point[0]+1, point[1]-1))
+        points.add((point[0]-1, point[1]))
+        points.add((point[0]-1, point[1]+1))
+        points.add((point[0]-1, point[1]-1))
+        points.add((point[0], point[1]+1))
+        points.add((point[0], point[1]-1))
+
+        return points
+
+
+    def _excludePoints(self, points):
+        """
+        :param points - tablica par (x,y):
+        :return:
+        """
+        for point in points:
+            if self._isGood(point):
+                self._notAvailable.add(point)
+
 def rec(sock, crlf):
     data_rec = b''
     while not data_rec.endswith(crlf):
         data_rec += sock.recv(1)
-    return data_rec.decode()
+    return data_rec[:-4].decode()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    s.connect(('localhost', 1769))
-
-    a = [[4,2,3,4,2,1,3],[1,2,3,4,5,6,7,8]]
-    s.sendall(str(a).encode())
-
-    message_rec = rec(s, CRLF)
-
-    print(message_rec)
-
-    s.close()
-except socket.error:
-    print ('Error')
-except KeyboardInterrupt:
-    s.close()
+# point = (1,2)
+# print(type(point))
+# point = (point[0]+1, point[1])
+# print(point)
+klasa = Map()
+klasa.generateMap()
+# try:
+#     s.connect(('localhost', 1769))
+#
+#     a = [[4,2,3,4,2,1,3],[1,2,3,4,5,6,7,8]]
+#
+#     hello = "201\r\nHello"
+#     klucz = "211\r\nMessage=My key\r\nKey=1231241\r\nKey-len=2048\r\n\r\n"
+#     s.sendall(str(hello).encode())
+#     message_rec = rec(s, CRLF)
+#     print(message_rec)
+#     s.sendall(klucz.encode())
+#     msg2=rec(s, CRLF)
+#     print(msg2)
+#     msg3=rec(s, CRLF)
+#     print(msg3)
+#
+#     board = Map()
+#     #board._printInto()
+#     mapa = pickle.dumps(board)
+#
+#     s.sendall(mapa)
+#     s.close()
+# except socket.error:
+#     print ('Error')
+# except KeyboardInterrupt:
+#     s.close()
--- main.py
+++ main.py
@@ -3,11 +3,14 @@ Serwer TCP
 '''
 import asyncio
 import random
+import numpy as np
 from concurrent.futures import ThreadPoolExecutor
+import pickle
+#import rsa
 from threading import Thread
 
 """
-200: dals[pdsap[l\r\n
+kod\r\nwiadomosc\r\nklucz..\r\n\r\n
 
 
 212
@@ -15,32 +18,99 @@ from threading import Thread
 "211\r\nMessage=hello,give me your key.There is my key\r\nKey=key\r\nKey-len:2048\r\n\r\n"
 
 """
-def Fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return Fib(n - 1) + Fib(n - 2)
+class Map:
+    def __init__(self):
+        self.mapa = np.zeros((10, 10))
+        self.pozycje = []
+        self._cztery = 1
+        self._trzy = 2
+        self._dwa = 3
+        self._jeden = 4
 
-class ProjectA:
 
+    def buildMap(self):
+
+        pass
+
+    def _printInfo(self):
+        msg_choice = "Masz do wyboru:\n" \
+              " czteromasztowców: {czt}\n" \
+              " trójmasztowców: {czy}\n" \
+              " dwumasztowców:{dwa}\n" \
+              " jednomasztowców: {jed}".format(
+            czt=self._cztery,
+            czy=self._trzy,
+            dwa=self._dwa,
+            jed=self._jeden)
+
+        msg_options = "\n ====Menu====\n" \
+                      "[1] rozstaw jednomasztowiec\n" \
+                      "[2] rozstaw dwumasztowiec\n" \
+                      "[3] rozstaw trójmasztwoiec\n" \
+                      "[4] roztaw czwórmasztwiec\n" \
+                      "[enter] Zaakceptuj (brak statków zostanie uzupełniony losowo)"
+        print(self.mapa)
+        print(msg_choice)
+        print(msg_options)
+
+
+def RamkaParser(data):
+    results = {}
+    data = data.split('\r\n')
+    try:
+
+        code = int(data[0])
+    except ValueError:
+        return '302\r\nBAD CODE ERROR'
+
+    results['code'] = code
+
+    #TODO:co jesli jest blad
+    if "Message=" == data[1][:8]:
+        results['message'] = data[1][8:]
+    if "Key=" == data[2][:4]:
+        results['key'] = data[2][4:]
+    if "Key-len=" == data[3][:8]:
+        results['key-len'] = int(data[3][8:])
+
+    return results
+
+class HelloHandshake:
     def __init__(self):
         self.codes = {}
         self.finished = False
+        self.clientPubKey = ""
 
     def accept_201(self, data):
-        if data == "201: Hello\r\n\r\n":
+        """
+        Funkcja przyjmuje wiadomość powitalną, zwraca ramkę z odpowiedzią
+        (klucz publiczny serwera i długość tego klucza)
+        """
+        if data == "201\r\nHello":
             self.codes[201] = True
             # read key from file
+            #TODO: add trycatch
+            publicKey = open("./publicKey.crt", "r")
 
-            message = "211\r\nMessage=hello,give me your key.There is my key\r\nKey={key}\r\nKey-len=2048\r\n\r\n".format(
-                key=""
-            )
+            message = "211\r\nMessage=Hello,give me your key.\r\n" \
+                      "Key={key}\r\nKey-len=2048\r\n\r\n".format(
+                key=publicKey.read())
+            return message
+
+    def accept_212(self, data):
+
+        headers = RamkaParser(data)
+        #assert headers['key-len'] >= 2048
+
+        if headers['code'] == 211 and self.codes[201]:
+            self.clientPubKey = headers['key']
+            self.finished = True
+            return '210\r\nMessage=Od teraz wszystkie wiadomosci sa szyfrowane\r\n\r\n' \
+                   '101\r\nInfo:Zbuduj mape\r\n\r\n'
+
+        return "301\r\nERROR"
             # klient 211\r\nMessage=There is my key\r\nKey={key}\r\nKey-len=2048\r\n\r\n
-    def accept_202(self, data):
-
-
+    #def accept_202(self, data):
 
 
 class BattleshipProtocol(asyncio.Protocol):
@@ -49,25 +119,27 @@ class BattleshipProtocol(asyncio.Protocol):
         self.addr = transport.get_extra_info('peername')
         self._rest = b''
         self.name = None
-        self.ProjectA = ProjectA()
+        self.HelloHandshake = HelloHandshake()
         print('Connection from {}'.format(self.addr))
 
     def data_received(self, data):
         data = self._rest + data
-        print(data)
 
-        if not self.ProjectA.finished:
+
+        if not self.HelloHandshake.finished:
             data = data.decode()
             if int(data[:3]) == 201:
-                message = self.ProjectA.accept_201(data)
+                message = self.HelloHandshake.accept_201(data)
                 self.transport.write(message.encode())
-            if int(data[:3]) == 202:
-                if self.ProjectA.codes[201]:
-                    self.ProjectA.accept_202(data)
-                else:
+            elif int(data[:3]) == 211:
+                message = self.HelloHandshake.accept_212(data)
+                self.transport.write(message.encode())
+                print(message)
+            return
 
-
-        # fib_num = int(data.decode())
+        mapa = pickle.loads(data)
+        mapa._printInfo()
+        # fib_num = int(data.de```code())
         #
         # task = asyncio.create_task(self.async_fib(fib_num))
 
@@ -88,11 +160,11 @@ class BattleshipProtocol(asyncio.Protocol):
     def connection_lost(self, ex):
         print('Client {} disconnected'.format(self.addr))
 
-    async def async_fib(self, fib_num):
-        task = await loop.run_in_executor(thread_pool, Fib, fib_num)
-
-        response = (str(task) + "\r\n").encode()
-        self.transport.write(response)
+    # async def async_fib(self, fib_num):
+    #     task = await loop.run_in_executor(thread_pool, Fib, fib_num)
+    #
+    #     response = (str(task) + "\r\n").encode()
+    #     self.transport.write(response)
 
 
 thread_pool = ThreadPoolExecutor()
